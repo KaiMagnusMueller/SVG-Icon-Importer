2409d74c180395a602beb5f2521b9d037ba98866
Kai Magnus Müller <ndsignin@outlook.de>
2022-03-20 18:51:02 +0100

    initial difference detection
    - moved hash generation to ui script

--- code.js
+++ code.js
@@ -16,8 +16,8 @@ if (nodes.length !== 0) {
         if (!data) {
             return;
         }
-        console.log(JSON.parse(data));
-        existingIcons.push(data);
+        // console.log(JSON.parse(data));
+        existingIcons.push(JSON.parse(data));
     });
     figma.ui.postMessage({ type: "loaded-nodes", data: existingIcons });
 }
@@ -68,7 +68,7 @@ figma.ui.onmessage = msg => {
             const pluginData = {
                 updateTime: Date.now,
                 name: element.name,
-                hash: cyrb53(element.svg)
+                hash: element.hash
             };
             svg.setPluginData("importedIcon", JSON.stringify(pluginData));
         });
@@ -76,15 +76,4 @@ figma.ui.onmessage = msg => {
     // Make sure to close the plugin when you're done. Otherwise the plugin will
     // keep running, which shows the cancel button at the bottom of the screen.
     //figma.closePlugin();
-};
-const cyrb53 = function (str, seed = 0) {
-    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
-    for (let i = 0, ch; i < str.length; i++) {
-        ch = str.charCodeAt(i);
-        h1 = Math.imul(h1 ^ ch, 2654435761);
-        h2 = Math.imul(h2 ^ ch, 1597334677);
-    }
-    h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
-    h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
-    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
 };
